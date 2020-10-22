940f9ae187ac46d887145a3b68744e09a688b28f
FelipeB4C <felipeb4c@gmail.com>
2020-10-22 00:45:34 -0300

    Adicionando validação Yup na função store e update

--- src/app/controllers/PokemonController.js
+++ src/app/controllers/PokemonController.js
@@ -1,3 +1,4 @@
+import * as Yup from 'yup'
 import Pokemon from '../models/Pokemon'
 
 class PokemonController {
@@ -12,15 +13,27 @@ class PokemonController {
   }
 
   async store (req, res) {
-    const { originalname: pokeimg, filename: path } = req.file
+    const schema = Yup.object().shape({
+      number: Yup.string().required(),
+      name: Yup.string().required(),
+      type: Yup.string().required(),
+      weakness: Yup.string().required(),
+      generation: Yup.string().required()
+    })
 
-    const { number, name, type, weakness, generation } = req.body
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Campos não preenchidos corretamente' })
+    }
 
     const pokeExists = await Pokemon.findOne({ where: { number: req.body.number } })
 
     if (pokeExists) {
       return res.status(400).json({ error: 'Pokemón já existe' })
     }
+
+    const { originalname: pokeimg, filename: path } = req.file
+
+    const { number, name, type, weakness, generation } = req.body
 
     const request = await Pokemon.create({ number, name, type, weakness, generation, pokeimg, path })
 
@@ -37,6 +50,18 @@ class PokemonController {
 
   async update (req, res) {
     const pokemon = await Pokemon.findByPk(req.params.id)
+
+    const schema = Yup.object().shape({
+      number: Yup.string(),
+      name: Yup.string(),
+      type: Yup.string(),
+      weakness: Yup.string(),
+      generation: Yup.string()
+    })
+
+    if (!(await schema.isValid(req.body))) {
+      return res.status(400).json({ error: 'Campos não preenchidos corretamente' })
+    }
 
     const file = req.file
 
